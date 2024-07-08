46be1bcb158b0ace84ec26f0c88f3ecc5968b664
Jeffrey Plewak <plewak.jeff@gmail.com>
2024-07-08 16:06:33 -0400

    Initial commit with Terraform and web scraper

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "project_id" {
+  description = "The ID of the GCP project"
+  type        = string
+}
+
+variable "region" {
+  description = "The GCP region"
+  type        = string
+  default     = "us-central1"
+}
+
